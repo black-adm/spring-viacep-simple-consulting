85a2f98b1a2a2b98d28d9f9a02fc9b8fc3c238bd
black-adm <mattheuscontato17@gmail.com>
2025-09-08 08:24:31 -0300

    fix: change to web client implementation

--- src/main/java/com/syscode42/viacep/controller/ViaCepController.java
+++ src/main/java/com/syscode42/viacep/controller/ViaCepController.java
@@ -1,26 +1,30 @@
 package com.syscode42.viacep.controller;
 
 import com.syscode42.viacep.dto.ViaCepResultDto;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("consulta-cep")
 public class ViaCepController {
 
+    private final WebClient client;
+
+    public ViaCepController(WebClient client) {
+        this.client = client;
+    }
+
     @GetMapping("{cep}")
-    public ViaCepResultDto search(@PathVariable("cep") String cep) {
-        RestTemplate restTemplate = new RestTemplate();
+    public Mono<ViaCepResultDto> search(@PathVariable("cep") String cep) {
+        String url = "https://viacep.com.br/ws/%s/json";
 
-        ResponseEntity<ViaCepResultDto> response =
-                restTemplate.getForEntity(
-                        String.format("https://viacep.com.br/ws/%s/json", cep),
-                        ViaCepResultDto.class);
-
-        return response.getBody();
+        return client.get()
+                .uri(String.format(url, cep))
+                .retrieve()
+                .bodyToMono(ViaCepResultDto.class);
     }
 }
